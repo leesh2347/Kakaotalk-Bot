4515a76bb3c485b9ca84ff81167b38f3b8aa5a78
leesh2347 <81743960+leesh2347@users.noreply.github.com>
2021-09-08 21:59:14 +0900

    Add files via upload

---
+++ choose.js
@@ -0,0 +1,34 @@
+const scriptName = "choose";
+/**
+ * (string) room
+ * (string) sender
+ * (boolean) isGroupChat
+ * (void) replier.reply(message)
+ * (boolean) replier.reply(room, message, hideErrorToast = false) // 전송 성공시 true, 실패시 false 반환
+ * (string) imageDB.getProfileBase64()
+ * (string) packageName
+ */
+function response(room, msg, sender, isGroupChat, replier, imageDB, packageName) {
+if(msg.includes(" vs "))
+{
+var arr=msg.split(" vs ")
+var r=Math.floor(Math.random()*arr.length);
+replier.reply("[루시] "+arr[r]+" (이) 가 더 좋을 것 같아요!");
+}
+}
+
+//아래 4개의 메소드는 액티비티 화면을 수정할때 사용됩니다.
+function onCreate(savedInstanceState, activity) {
+  var textView = new android.widget.TextView(activity);
+  textView.setText("Hello, World!");
+  textView.setTextColor(android.graphics.Color.DKGRAY);
+  activity.setContentView(textView);
+}
+
+function onStart(activity) {}
+
+function onResume(activity) {}
+
+function onPause(activity) {}
+
+function onStop(activity) {}
