3910527ae064864ac464595fd6365f86aab859b1
leesh2347 <81743960+leesh2347@users.noreply.github.com>
2021-12-21 22:39:17 +0900

    Add files via upload

--- maplegg.js
+++ maplegg.js
@@ -1,49 +1,40 @@
 const scriptName = "maplegg";
+FS = FileStream;
+//const kalingModule = require('kaling').Kakao();
 const { KakaoLinkClient } = require('kakaolink');
-const Kakao =new KakaoLinkClient("aaa","bbb");
-Kakao.login('aaa','bbb');
+const Kakao =new KakaoLinkClient("aaaaa","aaaaa");
+Kakao.login('aaaaa','aaaaa');
  
-function skipnick(sender) //단축어
-{
+var loc="sdcard/katalkbot/Bots/maplelog.json";
+if (FS.read(loc)==null) FS.write(loc, "{}");
+
+function recordnick(sender,nick){
+	var rd = JSON.parse(FS.read(loc));
+	if(rd[sender]==undefined) rd[sender] = {};
+	if(rd[sender][nick]==undefined)rd[sender][nick] = 0;
+	rd[sender][nick] = rd[sender][nick]+1;
+	FS.write(loc, JSON.stringify(rd))
+	
+}
+
+function recommendnick(sender,replier){
 	var n="";
-	if(sender.includes("디벨로이드")||sender=="이상훈")
-		n="디벨로이드";
-	else if(sender=="둔나")
-		n="손성";
-	else if(sender.includes("세렌디피티"))
-		n="l세렌디피티l";
-	else if(sender=="임주환")
-		n="뤠먼듀블";
-	else if(sender=="서연")
-		n="뎅잇";
-	else if(sender=="전태원")
-		n="홍색리트머스";
-	else if(sender=="ㅇㅅ")
-		n="혼남팡";
-	else if(sender=="윤근준")
-		n="션추";
-	else if(sender=="채하민")
-		n="단혜설";
-	else if(sender=="김태현")
-		n="Rucee";
-	else if(sender=="재우")
-		n="베얌";
-	else if(sender.includes("마법의쥐"))
-		n="마법의쥐";
-	else if(sender.includes("루시드키네"))
-		n="루시드키네o";
-	else if(sender.includes("싶으레"))
-		n="싶으레";
-	else if(sender.includes("따끈따끈밀크"))
-		n="따끈따끈밀크";
-	else if(sender.includes("최로"))
-		n="최로";
-	else if(sender.includes("우락키네"))
-		n="우락키네";
-	else if(sender.includes("온큐리"))
-		n="온큐리";
+	var rd = JSON.parse(FS.read(loc));
+	if(rd[sender]==undefined){
+		rd[sender] = {};
+		n="";
+	}
+	else{
+		result = [];
+		for (i in rd[sender]){
+			result.push(i+"/"+rd[sender][i]);
+		}
+		result.sort((a, b)=>a.split("/")[1] - b.split("/")[1]).reverse();
+		n=result[0].split("/")[0];
+	}
+	
 	return n;
-} 
+}
 
 function response(room, msg, sender, isGroupChat, replier, imageDB, packageName) {
   if(room=="바다 월드") return;
@@ -51,18 +42,38 @@ function response(room, msg, sender, isGroupChat, replier, imageDB, packageName)
 try{ 
       var nick = msg.split(" ")[1];
 	  if(nick==undefined)
-		  nick=skipnick(sender);
-      var img = org.jsoup.Jsoup.connect("https://maplestory.nexon.com/Ranking/World/Total?c=" +nick).get().select("#container > div > div > div:nth-child(4) > div.rank_table_wrap > table > tbody > tr.search_com_chk > td.left > span > img:nth-child(1)").get(0).select("img").attr("src");
-      var datar = org.jsoup.Jsoup.connect("https://maplestory.nexon.com/Ranking/World/Total?c=" +nick).get().select("tr.search_com_chk").select("dd");//공홈파싱
-      var job = datar.get(0).text().split("/")[1].split("\">\"")[0];//직업
-	  var serverimg=org.jsoup.Jsoup.connect("https://maplestory.nexon.com/Ranking/World/Total?c=" +nick).get().select("tr.search_com_chk").select("dt").select("img").attr("src")
-      var data = org.jsoup.Jsoup.connect("https://maplestory.nexon.com/Ranking/World/Total?c=" +nick).get().select("tr.search_com_chk").select("td");//공홈파싱
-      var lv = data.get(2).text().replace("Lv.", "");//td 3번째줄, 'Lv.'은 보기 좀 그래서 삭제
-      var exp = data.get(3).text();//td 4번째줄
-      var pop = data.get(4).text();//td 5번째줄
-      var gu = data.get(5).text();//td 6번째줄
-      var unity = "레벨: " + lv +" │ "+"인기도: "+ pop + "\n길드: "+ gu;
-      
+		  nick=recommendnick(sender,replier);
+	  if(nick=="") replier.reply("닉네임을 입력해 주세요.");
+	  else
+	  {
+		  recordnick(sender,nick);
+		  var isreboot=org.jsoup.Jsoup.connect("https://maplestory.nexon.com/Ranking/World/Total?c=" +nick).get().select("#container > div > div > div:nth-child(4)").text();
+		  if(isreboot!="랭킹정보가 없습니다.")
+		  {
+			  var img = org.jsoup.Jsoup.connect("https://maplestory.nexon.com/Ranking/World/Total?c=" +nick).get().select("#container > div > div > div:nth-child(4) > div.rank_table_wrap > table > tbody > tr.search_com_chk > td.left > span > img:nth-child(1)").get(0).select("img").attr("src");
+			  var datar = org.jsoup.Jsoup.connect("https://maplestory.nexon.com/Ranking/World/Total?c=" +nick).get().select("tr.search_com_chk").select("dd");//공홈파싱
+			  var job = datar.get(0).text().split("/")[1].split("\">\"")[0];//직업
+			  var serverimg=org.jsoup.Jsoup.connect("https://maplestory.nexon.com/Ranking/World/Total?c=" +nick).get().select("tr.search_com_chk").select("dt").select("img").attr("src")
+			  var data = org.jsoup.Jsoup.connect("https://maplestory.nexon.com/Ranking/World/Total?c=" +nick).get().select("tr.search_com_chk").select("td");//공홈파싱
+			  var lv = data.get(2).text().replace("Lv.", "");//td 3번째줄, 'Lv.'은 보기 좀 그래서 삭제
+			  var exp = data.get(3).text();//td 4번째줄
+			  var pop = data.get(4).text();//td 5번째줄
+			  var gu = data.get(5).text();//td 6번째줄
+			  var unity = "레벨: " + lv +" │ "+"인기도: "+ pop + "\n길드: "+ gu;
+		  }
+		  else
+		  {
+			  var img = org.jsoup.Jsoup.connect("https://maplestory.nexon.com/Ranking/World/Total?c=" +nick+"&w=254").get().select("#container > div > div > div:nth-child(4) > div.rank_table_wrap > table > tbody > tr.search_com_chk > td.left > span > img:nth-child(1)").get(0).select("img").attr("src");
+			  var datar = org.jsoup.Jsoup.connect("https://maplestory.nexon.com/Ranking/World/Total?c=" +nick+"&w=254").get().select("tr.search_com_chk").select("dd");//공홈파싱
+			  var job = datar.get(0).text().split("/")[1].split("\">\"")[0];//직업
+			  var serverimg=org.jsoup.Jsoup.connect("https://maplestory.nexon.com/Ranking/World/Total?c=" +nick+"&w=254").get().select("tr.search_com_chk").select("dt").select("img").attr("src")
+			  var data = org.jsoup.Jsoup.connect("https://maplestory.nexon.com/Ranking/World/Total?c=" +nick+"&w=254").get().select("tr.search_com_chk").select("td");//공홈파싱
+			  var lv = data.get(2).text().replace("Lv.", "");//td 3번째줄, 'Lv.'은 보기 좀 그래서 삭제
+			  var exp = data.get(3).text();//td 4번째줄
+			  var pop = data.get(4).text();//td 5번째줄
+			  var gu = data.get(5).text();//td 6번째줄
+			  var unity = "레벨: " + lv +" │ "+"인기도: "+ pop + "\n길드: "+ gu;
+		  }
 Kakao.sendLink(room, {
 "link_ver":"4.0",
 "template_id":(58792),
@@ -75,7 +86,7 @@ Kakao.sendLink(room, {
 'LINK':"u/"+nick
 }
 }, "custom")
-
+}
 }
 catch(e)
 {
