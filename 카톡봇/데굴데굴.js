38718b1b9ab363ebfea422cd6dc5470c82b398d6
leesh2347 <81743960+leesh2347@users.noreply.github.com>
2022-03-07 16:42:19 +0900

    Add files via upload

--- 카톡봇/데굴데굴.js
+++ 카톡봇/데굴데굴.js
@@ -24,9 +24,9 @@ var n1=0;
    }while(n4>10);
 var nick=sender;
 if(nick.includes("/")) nick=nick.split("/").join("_");
-var imgurl="https://res.cloudinary.com/leesh2347/image/upload/l_text:Arial_14_bold:"+encodeURIComponent(nick)+",co_rgb:ffffff,g_north_west,x_17,y_37/l_text:Arial_10_bold:"+n1;
-imgurl=imgurl+",co_rgb:000000,g_north_west,x_45,y_92/l_text:Arial_10_bold:"+n2+",co_rgb:000000,g_north_west,x_45,y_108/l_text:Arial_10_bold:"+n3+",co_rgb:000000,g_north_west,x_45,y_126/l_text:Arial_10_bold:"+n4;
-imgurl=imgurl+",co_rgb:000000,g_north_west,x_45,y_143/"+encodeURIComponent("주사위")+"_xyx3gp";
+var imgurl="https://res.cloudinary.com/leesh2347/image/upload/l_text:Arial_14_bold:"+encodeURIComponent(nick)+",co_rgb:ffffff,g_north_west,x_22,y_49/l_text:Arial_12_bold:"+n1;
+imgurl=imgurl+",co_rgb:000000,g_north_west,x_59,y_110/l_text:Arial_12_bold:"+n2+",co_rgb:000000,g_north_west,x_59,y_130/l_text:Arial_12_bold:"+n3+",co_rgb:000000,g_north_west,x_59,y_150/l_text:Arial_12_bold:"+n4;
+imgurl=imgurl+",co_rgb:000000,g_north_west,x_59,y_170/"+encodeURIComponent("주사위")+"_zap0t9";
 
 Kakao.sendLink(room, {
 "link_ver":"4.0",
