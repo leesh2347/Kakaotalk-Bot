433c26186174cc94eff56966d1d39bd1f29cad07
Lee Sang Hoon <81743960+leesh2347@users.noreply.github.com>
2025-12-09 16:43:34 +0900

    Create config.py

---
+++ iris_python/msgbot/Bots/db_modules/config.py
@@ -0,0 +1,10 @@
+import os
+
+# 예: MySQL
+SQLALCHEMY_DATABASE_URL = "mysql+pymysql://leesh2347:l6372325@123.456.789.123:3306/lucy_bot"
+
+# 예: MSSQL (ODBC Driver 17 기준)
+#SQLALCHEMY_DATABASE_URL = (
+#    "mssql+pyodbc://user:password@server_name_or_ip/lucy_bot"
+#    "?driver=ODBC+Driver+17+for+SQL+Server"
+#)
