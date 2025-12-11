3b05c5d8c67663eaaf8d45c7a2857b76ef46080a
Lee Sang Hoon <81743960+leesh2347@users.noreply.github.com>
2025-12-11 17:22:19 +0900

    Add files via upload

--- iris_python/msgbot/Bots/refresh.py
+++ iris_python/msgbot/Bots/refresh.py
@@ -1,6 +1,9 @@
+from msgbot.Bots.room_check.room_module import record_room_info
+
 message_count = {}
 
 def handle_message(chat):
+    room_id = chat.room.id
     room_name = chat.room.name
     sender = chat.sender.name
     
@@ -11,4 +14,5 @@ def handle_message(chat):
     
     if message_count[room_name] > 300:
         message_count[room_name] = 1
+        record_room_info(room_id, room_name)
         chat.reply("[루시] 누적 톡수 300개 누적!\n원활한 봇 작동을 위해 읽음처리됩니다.")
