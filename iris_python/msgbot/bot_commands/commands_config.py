0d11c1a40281bda407cba3ac1dc6227a61d3cb20
Lee Sang Hoon <81743960+leesh2347@users.noreply.github.com>
2025-12-23 10:51:35 +0900

    Add files via upload

--- iris_python/msgbot/bot_commands/commands_config.py
+++ iris_python/msgbot/bot_commands/commands_config.py
@@ -1,9 +1,19 @@
+PREFIX_MAPLEGG = ["@메이플", "!메이플"]
+
 PREFIX_MURUNG = ["@무릉", "!무릉"]
 PREFIX_UNION = ["@유니온", "!유니온"]
 PREFIX_ACHIEVE = ["@업적", "!업적"]
 PREFIX_ARTIFACT = ["@아티팩트", "!아티팩트"]
 
 PREFIX_LEVEL = ["@레벨", "!레벨"]
+
+PREFIX_HISTORY = ["@히스토리", "!히스토리"]
+
+PREFIX_LEVEL_HISTORY = ["@레벨히스토리", "!레벨히스토리"]
+
+PREFIX_STAT = ["@스탯", "!스탯"]
+
+PREFIX_MECHANG = ["@메창", "!메창"]
 
 PREFIX_HEXA = ["@헥사", "!헥사"]
 PREFIX_SIXTH_ALL = ["@6차", "!6차"]
