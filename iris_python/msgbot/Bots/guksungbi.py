51b101de2474eed6dd8e2017f4b086ab6d05691a
Lee Sang Hoon <81743960+leesh2347@users.noreply.github.com>
2026-02-02 14:12:09 +0900

    Add files via upload

--- iris_python/msgbot/Bots/guksungbi.py
+++ iris_python/msgbot/Bots/guksungbi.py
@@ -1,7 +1,8 @@
 import requests
 import numpy as np
-from msgbot.Bots.guk_data.sungbi_levdata import DATA_AFTER_210, DATA_101_TO_209, DATA_1_TO_100, DATA_HIGHMOUNTAIN, DATA_ANGLER, DATA_NIGHTMARE, DATA_EXP_COUPON, DATA_HIGH_EXP_COUPON, DATA_MONSTERPARK_EXTREME
-from msgbot.bot_commands.commands_config import PREFIX_CHOSUNGBI, PREFIX_GUKSUNGBI, PREFIX_TAESUNGBI, PREFIX_IKSUNGBI, PREFIX_SUNGBI_200, PREFIX_SUNGBI_210, PREFIX_SUNGBI_220, PREFIX_HIGHMOUNTAIN, PREFIX_ANGLER, PREFIX_NIGHTMARE, PREFIX_EXTREME_MONSTERPARK, PREFIX_EXPCOUPON, PREFIX_HIGHEXPCOUPON
+import math
+from msgbot.Bots.guk_data.sungbi_levdata import DATA_AFTER_210, DATA_101_TO_209, DATA_1_TO_100, DATA_HIGHMOUNTAIN, DATA_ANGLER, DATA_NIGHTMARE, DATA_EXP_COUPON, DATA_HIGH_EXP_COUPON, DATA_MONSTERPARK_EXTREME, DATA_EXPRESS_BOOSTER, DATA_MECHA_STRAWBERRY
+from msgbot.bot_commands.commands_config import PREFIX_CHOSUNGBI, PREFIX_GUKSUNGBI, PREFIX_TAESUNGBI, PREFIX_IKSUNGBI, PREFIX_SUNGBI_200, PREFIX_SUNGBI_210, PREFIX_SUNGBI_220, PREFIX_HIGHMOUNTAIN, PREFIX_ANGLER, PREFIX_NIGHTMARE, PREFIX_EXTREME_MONSTERPARK, PREFIX_EXPCOUPON, PREFIX_HIGHEXPCOUPON, PREFIX_EXPRESS_BOOSTER, PREFIX_MECHA_STRAWBERRY
 
 
 SUNGBI = [571115568,6120258214,22164317197,64359295696]
@@ -234,6 +235,47 @@ def high_exp_coupon(index, count):
 
             return f"{index}레벨에서 상급 EXP쿠폰 {c}개 사용 시 경험치 획득량 : {round(s, 3)}%"
 
+def express_booster(index, count):
+    if np.nan == index or index < 1 or index > 299:
+        return "레벨은 1~299 사이 숫자만 입력해 주세요."
+    else:
+        if index > 294 or index < 260:
+            return "익스프레스 부스터는 레벨 260 이상 294 이하인 캐릭터만 사용 가능합니다."
+        else:
+            c = 0
+            if np.nan == count or count is None:
+                c = 1
+            elif count < 1:
+                c = 1
+            else:
+                c = int(count)
+
+            l = DATA_AFTER_210[index - 210]
+            e = DATA_EXPRESS_BOOSTER[index - 260]
+
+            s = e/l*100
+            s_c = s*c
+
+            t = math.ceil(100 / s)
+
+            return f"{index}레벨에서 익스프레스 부스터 몬스터 {c}마리 처치 시 경험치 획득량 : {round(s_c, 3)}%\n\n1레벨업에 필요한 사냥 마릿수: {t}"
+
+def mecha_strawberry(index):
+    if np.nan == index or index < 1 or index > 299:
+        return "레벨은 1~299 사이 숫자만 입력해 주세요."
+    else:
+        if index < 280:
+            return "메카 딸기 농장은 280레벨 이상의 캐릭터만 입장 가능합니다."
+        else:
+            l = DATA_AFTER_210[index - 210]
+            e = DATA_MECHA_STRAWBERRY[index - 280]
+
+            s = e/l*100
+
+            t = math.ceil(100 / s)
+
+            return f"{index}레벨에서 메카딸기농장 1회 입장 시 경험치 획득량 : {round(s, 3)}%\n\n1레벨업에 필요한 티켓 수: {t}장"
+
 def handle_message(chat):
     #초성비
     if any(prefix in chat.message.msg for prefix in PREFIX_CHOSUNGBI):
@@ -349,4 +391,26 @@ def handle_message(chat):
                 c = 1
 
             res = high_exp_coupon(n, c)
+            chat.reply(res)
+
+    #익부
+    if any(prefix in chat.message.msg for prefix in PREFIX_EXPRESS_BOOSTER):
+        parts = chat.message.msg.split(" ")
+        if len(parts) > 1 and parts[1].isdigit():
+            n = int(parts[1])
+
+            if len(parts) > 2 and parts[2].isdigit():
+                c = int(parts[2])
+            else:
+                c = 1
+
+            res = express_booster(n, c)
+            chat.reply(res)
+
+    #메카딸기
+    if any(prefix in chat.message.msg for prefix in PREFIX_MECHA_STRAWBERRY):
+        parts = chat.message.msg.split(" ")
+        if len(parts) > 1 and parts[1].isdigit():
+            n = int(parts[1])
+            res = mecha_strawberry(n)
             chat.reply(res)
