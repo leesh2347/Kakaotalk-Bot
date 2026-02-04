c95f1b4d5d5751c2aa7985b5b24b6d935418b3c0
Lee Sang Hoon <81743960+leesh2347@users.noreply.github.com>
2026-02-04 16:08:15 +0900

    Add files via upload

--- iris_python/msgbot/Bots/food.py
+++ iris_python/msgbot/Bots/food.py
@@ -4,7 +4,7 @@ import os
 import random
 
 from msgbot.Bots.maple_nickskip.nickskip_module import _load_data, _save_data
-from msgbot.bot_commands.commands_config import PREFIX_FOOD_ADD, PREFIX_FOOD_DELETE
+from msgbot.bot_commands.commands_config import PREFIX_FOOD_ADD, PREFIX_FOOD_DELETE, PREFIX_JOB_ADD, PREFIX_JOB_DELETE, PREFIX_FOOD, PREFIX_JOB
 
 # banned_rooms.json의 절대 경로 계산
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))  # msgbot/
@@ -23,35 +23,52 @@ with open(FILTER_FILE, "r", encoding="utf-8") as f:
 # 금지 방 목록 로드
 BANNED_PLAY_ROOMS = filter_data["banrooms"]["play"]
 
-def add_food(new_food):
-    foods_arr = _load_data(DATA_FILE_DIR)["food"]
+def add_food(new_food, add_type):
+    foods_arr = _load_data(DATA_FILE_DIR)[f'{add_type}']
 
     space = "\u200b" * 500
 
     if new_food not in foods_arr:
         foods_arr.append(new_food)
-        record_to_data_file(DATA_FILE_DIR, "food", foods_arr)
-        return f'[루시] 뭐먹지 메뉴 등록 완료: {new_food}\n{space}\n현재 등록된 뭐먹지 목록\n{",".join(foods_arr)}'
+        record_to_data_file(DATA_FILE_DIR, add_type, foods_arr)
+        if add_type =="food":
+            res_text = f'[루시] 뭐먹지 메뉴 등록 완료: {new_food}\n{space}\n현재 등록된 뭐먹지 목록\n{",".join(foods_arr)}'
+        else:
+            res_text = f'[루시] 뭐하지 할일 등록 완료: {new_food}\n{space}\n현재 등록된 뭐하지 목록\n{",".join(foods_arr)}'
+        return res_text
     else:
-        return f'[루시] 이미 등록된 메뉴입니다: {new_food}\n{space}\n현재 등록된 뭐먹지 목록\n{",".join(foods_arr)}'
+        if add_type =="food":
+            res_text = f'[루시] 이미 등록된 메뉴입니다: {new_food}\n{space}\n현재 등록된 뭐먹지 목록\n{",".join(foods_arr)}'
+        else:
+            res_text = f'[루시] 이미 등록된 할일입니다: {new_food}\n{space}\n현재 등록된 뭐하지 목록\n{",".join(foods_arr)}'
+        return res_text
 
-def delete_food(new_food):
-    foods_arr = _load_data(DATA_FILE_DIR)["food"]
+def delete_food(new_food, del_type):
+    foods_arr = _load_data(DATA_FILE_DIR)[f'{del_type}']
 
     space = "\u200b" * 500
 
     if new_food in foods_arr:
         idx = foods_arr.index(new_food)
         del foods_arr[idx]
-        record_to_data_file(DATA_FILE_DIR, "food", foods_arr)
-        return f'[루시] 뭐먹지 메뉴 삭제 완료: {new_food}\n{space}\n현재 등록된 뭐먹지 목록\n{",".join(foods_arr)}'
+        record_to_data_file(DATA_FILE_DIR, del_type, foods_arr)
+        res_text = ''
+        if del_type =="food":
+            res_text = f'[루시] 뭐먹지 메뉴 삭제 완료: {new_food}\n{space}\n현재 등록된 뭐먹지 목록\n{",".join(foods_arr)}'
+        else:
+            res_text = f'[루시] 뭐하지 할일 삭제 완료: {new_food}\n{space}\n현재 등록된 뭐하지 목록\n{",".join(foods_arr)}'
+        return res_text
     else:
-        return f'[루시] 등록되어있지 않은 메뉴입니다: {new_food}\n{space}\n현재 등록된 뭐먹지 목록\n{",".join(foods_arr)}'
+        if del_type =="food":
+            res_text = f'[루시] 등록되어있지 않은 메뉴입니다: {new_food}\n{space}\n현재 등록된 뭐먹지 목록\n{",".join(foods_arr)}'
+        else:
+            res_text = f'[루시] 등록되어있지 않은 할일입니다: {new_food}\n{space}\n현재 등록된 뭐하지 목록\n{",".join(foods_arr)}'
+        return res_text
 
-def choose_food(msg):
+def choose_food(msg, choose_type):
     parts = msg.split(" ")
 
-    foods_arr = _load_data(DATA_FILE_DIR)["food"]
+    foods_arr = _load_data(DATA_FILE_DIR)[f'{choose_type}']
 
     if len(parts) > 1 and parts[1].isdigit():
         n = int(parts[1])
@@ -61,10 +78,16 @@ def choose_food(msg):
             for i in range(1,n+1):
                 t = foods_arr[random.randint(1, len(foods_arr))-1]
                 res.append(t)
-            return f"{chat.sender.name}님의 추천 메뉴는\n\n{','.join(res)}"
+            if choose_type =="food":
+                return f"{chat.sender.name}님의 추천 메뉴는\n\n{','.join(res)}"
+            else:
+                return f"{chat.sender.name}님의 추천 할일은\n\n{','.join(res)}"
     else:
         t = random.randint(1, len(foods_arr))
-        return f"선택장애 {chat.sender.name}님!\n메뉴는 [{foods_arr[t-1]}]입니다 ^.^"
+        if choose_type =="food":
+            return f"선택장애 {chat.sender.name}님!\n메뉴는 [{foods_arr[t-1]}]입니다 ^.^"
+        else:
+            return f"심심하신 {chat.sender.name}님!\n[{foods_arr[t-1]}] 추천드립니다 ^.^"
 
 def handle_message(chat):
     
@@ -77,7 +100,7 @@ def handle_message(chat):
 
         if len(parts) > 1:
             new_food = chat.message.msg[7:]
-            res = add_food(new_food)
+            res = add_food(new_food, "food")
             chat.reply(res)
         else:
             chat.reply("메뉴를 입력해 주세요.")
@@ -87,13 +110,41 @@ def handle_message(chat):
 
         if len(parts) > 1:
             new_food = chat.message.msg[7:]
-            res = delete_food(new_food)
+            res = delete_food(new_food, "food")
             chat.reply(res)
 
         else:
             chat.reply("메뉴를 입력해 주세요.")
-    elif '뭐먹지' in chat.message.msg or 'ㅁㅁㅈ' in chat.message.msg:
-        res = choose_food(chat.message.msg)
+
+    if any(prefix in chat.message.msg for prefix in PREFIX_JOB_ADD):
+
+        parts = chat.message.msg.split(" ")
+
+        if len(parts) > 1:
+            new_food = chat.message.msg[7:]
+            res = add_food(new_food, "job")
+            chat.reply(res)
+        else:
+            chat.reply("할일을 입력해 주세요.")
+
+    elif any(prefix in chat.message.msg for prefix in PREFIX_JOB_DELETE):
+        parts = chat.message.msg.split(" ")
+
+        if len(parts) > 1:
+            new_food = chat.message.msg[7:]
+            res = delete_food(new_food, "job")
+            chat.reply(res)
+
+        else:
+            chat.reply("할일을 입력해 주세요.")
+
+    elif any(prefix in chat.message.msg for prefix in PREFIX_FOOD):
+        res = choose_food(chat.message.msg, "food")
         chat.reply(res)
+
+    elif any(prefix in chat.message.msg for prefix in PREFIX_JOB):
+        res = choose_food(chat.message.msg, "job")
+        chat.reply(res)
+
 
 
