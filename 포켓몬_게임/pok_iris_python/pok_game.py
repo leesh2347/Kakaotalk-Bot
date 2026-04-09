b1c720138ebb83dfe97893559be9125735f8a11f
Lee Sang Hoon <81743960+leesh2347@users.noreply.github.com>
2026-04-09 13:08:32 +0900

    닉네임 특수문자 예외처리

--- 포켓몬_게임/pok_iris_python/pok_game.py
+++ 포켓몬_게임/pok_iris_python/pok_game.py
@@ -44,7 +44,7 @@ def handle_message(chat):
     """
     room = chat.room.name
     msg = chat.message.msg
-    sender = chat.sender.name.replace(" ", "_")
+    sender = chat.sender.name.replace(" ", "_").replace("/", "_").replace("\\", "_")
     msg_first = msg.split(" ")[0] if msg else ""
 
     # Room filter - only respond in Pokemon room
